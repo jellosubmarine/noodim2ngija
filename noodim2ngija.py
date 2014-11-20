540be9a17547c7ea6d5fb51503a1d78ba8dc587a
Madis <madis1995@gmail.com>
2014-11-20 12:10:56 +0200

    vaheseis

--- noodim2ngija.py
+++ noodim2ngija.py
@@ -5,7 +5,6 @@ pygame.init()
 pygame.mixer.music.set_volume(1.0)
 screen = pygame.display.set_mode([800,600])
 pygame.display.set_caption('Noodimängija')
-
 #taust
 background = pygame.Surface(screen.get_size())
 background = background.convert()
@@ -19,11 +18,12 @@ noodid = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B']
 
 #noodijoonestik
 for i in range(5):
-    pygame.draw.line(screen, (0,0,0),(0,250 + i*25),(800,250 + i*25))
+    pygame.draw.line(screen, (0,0,0),(0,250 + i*24),(800,250 + i*24))
 pygame.display.flip()
 
 def täitmine():
-    kast = pygame.Surface((230,150))
+    kast = pygame.Surface((150,230))
+    kast = kast.convert()
     kast.fill((255,255,255))
     screen.blit(kast, (0,0))
     pygame.display.flip()
@@ -39,5 +39,29 @@ def viiulivõti():
     screen.blit(viiul, (50,20))
     pygame.display.flip()
 
+    
+def noodid():
+    for i in range(13):
+        pygame.draw.circle(screen, (0,0,0), (100 + i*40,370 - i*12), 12,2)
+    pygame.draw.line(screen, (0,0,0), (85, 370), (115,370), 1)
+    pygame.draw.line(screen, (0,0,0), (85 + 12*40, 370-6*24), (115 + 12*40,370-6*24), 1)
+    pygame.display.flip()
+noodid()
+pygame.mixer.music.load('F4.wav')
+pygame.mixer.music.play()
+viiulivõti()
+
+pygame.time.wait(5000)
+pygame.mixer.music.load('F2.wav')
+pygame.mixer.music.play()
+bassivõti()
+
+
+
+"""def hiireklikk(xalg,yalg,x,y):
+    for i in pygame.event.get():
+        if i.type == pygame.MOUSEBUTTONDOWN:
+            koht = pygame.mouse.get_pos()
+            if koht"""
 
     
--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 Sisestad noodistikust andmed graafiliselt
 mängib õige .wavi
+
+olemas on viiuli- ja bassivõti, mixer, noodid ilma frame'ideta
